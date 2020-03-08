c1898bb6439fd7aa387488dbcc36c45a225c917f
JP Hayes <john.has.come@gmail.com>
2020-03-08 14:45:56 -0600

    Fix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests, json
+import requests, json, os
 
 def index(request):
     #request_json = request.get_json(silent=True)
@@ -6,6 +6,13 @@ def index(request):
 
     #if request_json and 'name' in request_json:
     #    name = request_json['name']
+
+    ytkey = os.environ.get("ytkey")
+    mykey = os.environ.get("mykey")
+    if ytkey is None:
+        return "No YouTube key."
+    if mykey is None:
+        return "No API key."
 
     key = None
     username = None
@@ -15,22 +22,34 @@ def index(request):
         username = request.args["username"]
 
     if username is None:
-        return "No username param"
+        return "No username param."
     if key is None:
-        return "No key param"
-
-
+        return "No key param."
+    if key != mykey:
+        return "Bad key."
 
     try:
-        response = requests.get(
+        channel_response = requests.get(
             "https://www.googleapis.com/youtube/v3/channels",
             params={
-                "key": key,
+                "key": ytkey,
                 "part": "snippet,contentDetails,statistics",
                 "forUsername": username
             },
             headers={
-                # "Authorization": "Bearer " + key,
+                "Accept": "application/json"
+            }
+        )
+        channel_json = channel_response.json()
+        next_playlist = channel_json["data"]["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+        playlist_response = requests.get(
+            "https://www.googleapis.com/youtube/v3/playlists",
+            params={
+                "key": ytkey,
+                "part": "snippet,contentDetails,status,player,id",
+                "id": next_playlist
+            },
+            headers={
                 "Accept": "application/json"
             }
         )
@@ -38,8 +57,11 @@ def index(request):
         return str(e)
 
     jresponse = {
-        "url": response.request.url,
-        "data": response.json()
+        "channel_url": channel_response.request.url,
+        "playlist_url": playlist_response.request.url,
+        "playlist_id": next_playlist,
+        "channel_json": channel_json,
+        "playlist_json": playlist_response.json()
     }
 
     return json.dumps(jresponse, indent=4)
